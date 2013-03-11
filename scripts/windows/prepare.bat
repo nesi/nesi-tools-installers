37b3c5708ed8a4084c12402bafc87b42608d2363
Markus Binsteiner <makkus@gmail.com>
2013-03-11 14:45:57 +1300

    fix java path

--- scripts/windows/prepare.bat
+++ scripts/windows/prepare.bat
@@ -26,21 +26,25 @@ if NOT EXIST "%getdown_txt_target%" (
 
 set nesi_png=%dir%\nesi.png
 set nesi_png_source=%GRISU_HOME%\resources\nesi.png
+
 if NOT EXIST "%nesi_png%" (
     COPY "%nesi_png_source%" "%dir%\nesi.png"
 )
-set java_vm_location=%APPDATA%\java_vm
 
-if NOT EXIST %java_vm_location% (
+set java_vm_location=%dir%\java_vm
+
+if NOT EXIST "%java_vm_location%" (
 	set JAVA_EXE="%GRISU_HOME%\java_vm\bin\java.exe"
 ) else (
 	set JAVA_EXE="%java_vm_location%\bin\java.exe"
 )
 
+
 if NOT EXIST %JAVA_EXE% (
 	set JAVA_EXE=java
 )
 	
+
 set GRID_CLIENT_CLASSPATH=%dir%\grid-client-core.jar;%dir%\grid-client-dependencies.jar;%dir%\local-backend.jar
 
 set GETDOWN_DIR=%dir%
@@ -50,9 +54,5 @@ if EXIST "%dir%\local-backend.dat" (
 ) else (
 	set JAVA_OPTS=-Xmx128m
 )
-	
-REM @echo "Using java: %JAVA_EXE%"
+
 set X509_USER_PROXY=%USERPROFILE%\.grid\grid.proxy
-
-
-
