d3486748fb87f6024e3d62f27fa673d921b00089
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2013-03-28 09:46:39 +1300

    Added goji

---
+++ scripts/windows/goji.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+CALL prepare.bat
+
+@echo Checking for updates. This might take a while...
+%JAVA_EXE% -Dsilent -jar "%GRISU_HOME%\resources\getdown-client.jar" "%dir%"
+
+@echo Using proxy path: "%X509_USER_PROXY%"
+	
+%JAVA_EXE% %JAVA_OPTS% -cp "%GRID_CLIENT_CLASSPATH%" nz.org.nesi.goji.view.GojiCli %*
+
