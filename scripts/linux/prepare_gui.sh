25362ac2fc690c4023fc9c6d1ff3c261bf764885
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2013-10-31 11:13:47 +1300

    Added app-manage package

---
+++ scripts/linux/prepare_gui.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export GETDOWN_DIR="$DIR/../launch"
+
+
+export JAVA_OPTS="-Xms64m"
+
+timestamp_file=$GETDOWN_DIR/last_update
+
+# checking whether user wants local backend
+ARGS="$@"
+shopt -s nocasematch
+if [[ ($ARGS =~ -b[\ \ *][local]) || ($ARGS =~ --backend[\ \ *][local]) ]]; then
+	touch "$GETDOWN_DIR/local-backend.dat"
+	#echo "Checking whether local backend is available and up to date..."
+	java -Dsilent=true -jar "$GETDOWN_DIR/getdown-client.jar" "$GETDOWN_DIR"
+	touch "$timestamp_file"
+	JAVA_OPTS="-Djava.awt.headless=true -Xms128m -Xmx512m"
+fi
+
+if [ ! -f "$GETDOWN_DIR/grid-client-core.jar" ]
+then
+    #echo "First time run, downloading main application. This might take a little while..."
+    java -Dsilent=true -jar "$GETDOWN_DIR/getdown-client.jar" "$GETDOWN_DIR"
+else
+    # checking whether last update was in the last 24 hours
+    if [[ ( ! -f "$timestamp_file" ) || ( -n `find "$timestamp_file" -mmin +1440` ) ]]
+    then
+	#echo "Checking for updates. This might take a while..."
+	java -Dsilent=true -jar "$GETDOWN_DIR/getdown-client.jar" "$DIR"
+    #else
+	#echo "Not checking for update, already checked today..."
+    fi
+fi
+
+touch "$timestamp_file"
+
+export X509_USER_PROXY="$HOME/.grid/grid.proxy"
+
+# check whether we have our own jvm
+if [ -f "$GETDOWN_DIR/java_vm/bin/java" ]
+then
+	export JAVA_EXE="$DIR/../launch/java_vm/bin/java"
+else
+	export JAVA_EXE=`which java`
+fi
+
+export GRID_CLIENT_CLASSPATH="$GETDOWN_DIR/grid-client-core.jar:$GETDOWN_DIR/grid-client-dependencies.jar:$GETDOWN_DIR/local-backend.jar"
---
+++ scripts/windows/app-manage.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+CALL prepare.bat
+
+REM @echo Checking for updates. This might take a while...
+%JAVA_EXE% -Dsilent -jar "%GRISU_HOME%\resources\getdown-client.jar" "%dir%"
+
+REM @echo Using proxy path: "%X509_USER_PROXY%"
+
+%JAVA_EXE% %JAVA_OPTS% -cp "%GRID_CLIENT_CLASSPATH%" nz.org.nesi.appmanage.AppManage %*
+
